5c389c6c80cc8c35d8f1a7f0f5609a4da7589cbe
Agrim <agrim19229@iiitd.ac.in>
2021-05-13 11:24:02 +0530

    add port

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ app.get('/', (req, res, next) => {
     return res.render('home');
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Listening on port 3000');
 });
