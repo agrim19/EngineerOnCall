7b5c9da87ad06e532379aa1a106feff9253f5f2a
Agrim <agrim19229@iiitd.ac.in>
2021-08-13 18:55:56 +0530

    add 404 page and fix view error

--- app.js
+++ app.js
@@ -1,51 +1,60 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const nodemailer = require('nodemailer');
-const dotenv = require('dotenv');
+const nodemailer = require("nodemailer");
+const dotenv = require("dotenv");
 
-require('dotenv').config();
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
-app.use(express.urlencoded({ extended: true }));
+require("dotenv").config();
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({extended: true}));
 
-app.get('/', (req, res, next) => {
-    return res.render('./views/home');
+// app.get("*", (req, res, next) => {
+//     if (req.headers["x-forwarded-proto"] != "https")
+//         res.redirect("https://engineeroncall.in" + req.url);
+//     else next();
+// });
+
+app.get("/", (req, res, next) => {
+    return res.render("home");
 });
-app.post('/', (req, res, next) => {
+app.post("/", (req, res, next) => {
     let bodyText =
-        'There is a new response to the form on your Engineer On Call website. The details of the person are as follows:\n\n1. Name: ' +
+        "There is a new response to the form on your Engineer On Call website. The details of the person are as follows:\n\n1. Name: " +
         req.body.name +
-        '\n2. Phone Number:' +
+        "\n2. Phone Number:" +
         req.body.number +
-        '\n3. Site Address: ' +
+        "\n3. Site Address: " +
         req.body.address +
-        '\n 4. Package: ' +
+        "\n 4. Package: " +
         req.body.type;
     const optionalBodyText =
-        '\n5. Time:' + req.body.time + '\n6. Date:' + req.body.date;
-    if (req.body.type == 'Single Visit') {
+        "\n5. Time:" + req.body.time + "\n6. Date:" + req.body.date;
+    if (req.body.type == "Single Visit") {
         bodyText += optionalBodyText;
     }
     const smtpTransport = nodemailer.createTransport({
-        service: 'Gmail',
+        service: "Gmail",
         auth: {
             user: process.env.GMAILUSER,
             pass: process.env.GMAILPW,
         },
     });
     const mailOptions = {
-        to: 'info@engineeroncall.in',
+        to: "info@engineeroncall.in",
         from: process.env.GMAILUSER,
-        subject: 'New Response To Engineer On Call Form',
+        subject: "New Response To Engineer On Call Form",
         text: bodyText,
     };
     smtpTransport.sendMail(mailOptions, (err) => {
         if (err) console.log(err);
         else console.log(bodyText);
-        return res.render('home');
+        return res.render("home");
     });
 });
 
+app.use(function (req, res) {
+    res.status(404).render("fallback");
+});
 app.listen(process.env.PORT || 3000, () => {
-    console.log('Listening on port 3000');
+    console.log("Listening on port 3000");
 });
